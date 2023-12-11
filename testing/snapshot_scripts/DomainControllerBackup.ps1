5175136af3f9401c9de6745403f92037ab385fd7
Clint Baxley <clint.baxley-ctr@ecstech.com>
2023-12-11 06:34:35 -0500

    Create polling for long running jobs

--- testing/snapshot_scripts/DomainControllerBackup.ps1
+++ testing/snapshot_scripts/DomainControllerBackup.ps1
@@ -22,6 +22,7 @@ $vaultName = "$vmName-$version-"
 $remainingChars = 24 - $vaultName.Length
 
 # If the vault name is less than the maximum length, append random characters
+Write-Output "Generating random characters to create a new vault name"
 if ($remainingChars -gt 0) {
     # Generate a random string of the remaining length
     $randomString = -join ((48..57) + (65..90) + (97..122) | Get-Random -Count $remainingChars | ForEach-Object { [char]$_ })
@@ -31,6 +32,7 @@ if ($remainingChars -gt 0) {
 }
 
 # Get the location and storage account of the VM
+Write-Output "Getting details for ${vmName} to determine location and storage account"
 $vmDetails = az vm show `
     --name $vmName `
     --resource-group $resourceGroupName `
@@ -44,12 +46,14 @@ $storageAccountName = $vmDetails[1]
 $snapshotResourceGroupName = "TestbedAssets-${vmLocation}"
 
 # Create a Recovery Services vault in its resource group
+Write-Output "Creating a backup vault"
 az backup vault create `
     --name $vaultName `
     --resource-group $resourceGroupName `
     --location $vmLocation
 
 # Get vault details
+Write-Output "Getting vault details"
 $vaultId = az backup vault show `
     --name $vaultName `
     --resource-group $resourceGroupName `
@@ -57,6 +61,7 @@ $vaultId = az backup vault show `
     -o tsv
 
 # Set backup policy
+Write-Output "Setting default backup policy"
 $policyName = "DefaultPolicy"
 az backup policy set `
     --name $policyName `
@@ -65,6 +70,7 @@ az backup policy set `
     --policy (az backup policy list ` --resource-group $resourceGroupName ` --vault-name $vaultName ` --query "[?properties.datasourceType=='AzureIaasVM'].name" --output json)
 
 # Enable backup for the VM
+Write-Output "Setting backup protection for ${vmName}"
 az backup protection enable-for-vm `
     --vault-name $vaultName `
     --resource-group $resourceGroupName `
@@ -73,6 +79,7 @@ az backup protection enable-for-vm `
     --policy-name $policyName
 
 # Trigger the initial backup
+Write-Output "Backing up ${vmName}"
 $backupJob = az backup protection backup-now `
     --resource-group $resourceGroupName `
     --vault-name $vaultName `
@@ -81,12 +88,23 @@ $backupJob = az backup protection backup-now `
     --retain-until (Get-Date).AddDays(30).ToString("yyyy-MM-dd") `
     --backup-management-type AzureIaasVM
 
+
 $recoveryPointName = $backupJob.properties.entityFriendlyName
+
+# Polling for the completion of the backup job
+$backupJobId = $backupJob.Id
+do {
+    Start-Sleep -Seconds 30
+    $backupJobStatus = az backup job show --id $backupJobId --query "properties.status" -o tsv
+    Write-Output "Waiting for backup job to complete. Current status: $backupJobStatus"
+} while ($backupJobStatus -eq "InProgress" -or $backupJobStatus -eq "InProgress")
+
 
 $diskName = "$vmName-$version-disk"
 
 # Restore VM to create a new managed disk
-az backup restore restore-disks `
+Write-Output "Restoring disks for ${vmName}"
+$restoreJob = az backup restore restore-disks `
     --resource-group $resourceGroupName `
     --vault-name $vaultName `
     --container-name $vmName `
@@ -96,8 +114,17 @@ az backup restore restore-disks `
     --target-resource-group $resourceGroupName `
     --disk-name $diskName
 
+# Polling for the completion of the restore job
+$restoreJobId = $restoreJob.Id
+do {
+    Start-Sleep -Seconds 30
+    $restoreJobStatus = az backup job show --id $restoreJobId --query "properties.status" -o tsv
+    Write-Output "Waiting for restore job to complete. Current status: $restoreJobStatus"
+} while ($restoreJobStatus -eq "InProgress" -or $restoreJobStatus -eq "InProgress")
+
 # Take a snapshot of the created disk
 $snapshotName = "$vmName-$version"
+Write-Output "Creating a snapshot of the restored disk so we can copy to regions"
 az snapshot create `
     --name $snapshotName `
     --resource-group `
@@ -106,6 +133,7 @@ az snapshot create `
     --location $vmLocation
 
 # Move the snapshot to the snapshot resource group if necessary
+Write-Output "Moving the snapshot to the ${snapshotResourceGroupName} resource group"
 az resource move `
     --destination-group $snapshotResourceGroupName `
     --ids "/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.Compute/snapshots/$snapshotName"
