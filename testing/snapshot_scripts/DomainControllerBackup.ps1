cefc8413e36261949e9554efa298d7097ba2162d
Clint Baxley <clint.baxley-ctr@ecstech.com>
2023-12-13 05:03:56 -0500

    Get lists of restores and disks

--- testing/snapshot_scripts/DomainControllerBackup.ps1
+++ testing/snapshot_scripts/DomainControllerBackup.ps1
@@ -18,7 +18,6 @@ $ErrorActionPreference = 'Stop'
 # Get the current Azure subscription ID
 $subscriptionId = az account show --query "id" -o tsv
 
-
 # Get the location and storage account of the VM
 Write-Output "Getting details for ${vmName} to determine location and storage account"
 $vmLocation = (az vm show --name $vmName --resource-group $resourceGroupName --query "location" -o tsv).Trim()
@@ -26,22 +25,22 @@ $vmLocation = (az vm show --name $vmName --resource-group $resourceGroupName --q
 # Construct the final snapshot resource group name
 $snapshotResourceGroupName = "TestbedAssets-${vmLocation}"
 
-function Get-ValidStorageAccountName {
+function Get-ValidStorageName {
     param(
         [string]$baseName,
         [string]$version
     )
 
     # Function to sanitize and format the storage account name
-    function Sanitize-ForStorageAccount {
+    function Sanitize-ForStorageName {
         param([string]$namePart)
         # Remove invalid characters, convert to lower case, and trim to max length
         return ($namePart -replace '[^a-z0-9]', '').ToLower()
     }
 
     # Sanitize base name and version
-    $cleanBaseName = Sanitize-ForStorageAccount -namePart $baseName
-    $cleanVersion = Sanitize-ForStorageAccount -namePart $version
+    $cleanBaseName = Sanitize-ForStorageName -namePart $baseName
+    $cleanVersion = Sanitize-ForStorageName -namePart $version
 
     # Start constructing the storage account name
     $storageAccountName = $cleanBaseName + $cleanVersion
@@ -61,7 +60,7 @@ function Get-ValidStorageAccountName {
     return $storageAccountName
 }
 
-$storageAccountName = Get-ValidStorageAccountName -baseName $vmName -version $version
+$storageAccountName = Get-ValidStorageName -baseName $vmName -version $version
 
 Write-Output "Using location ${vmLocation} and storage account ${storageAccountName}"
 
@@ -73,10 +72,8 @@ az storage account create `
     --sku Standard_LRS `
     --kind StorageV2
 
-
-
 # Create a Recovery Services vault in its resource group
-$vaultName = Get-ValidStorageAccountName -baseName $vmName -version $version
+$vaultName = Get-ValidStorageName -baseName $vmName -version $version
 Write-Output "Creating a backup vault"
 az backup vault create `
     --name $vaultName `
@@ -109,8 +106,6 @@ az backup policy set `
     --resource-group $resourceGroupName `
     --policy "@policy.json"
 
-
-
 # Enable backup for the VM
 Write-Output "Setting backup protection for ${vmName}: ${vaultName} ${resourceGroupName} ${vmName} ${policyName}"
 az backup protection enable-for-vm `
@@ -118,7 +113,6 @@ az backup protection enable-for-vm `
     --resource-group $resourceGroupName `
     --vm $vmName `
     --policy-name $policyName
-
 
 # Trigger the initial backup
 Write-Output "Backing up ${vmName}: ${resourceGroupName} ${vaultName} ${vmName}"
@@ -135,7 +129,6 @@ $backupJobJson = az backup protection backup-now `
 $backupJob = $backupJobJson | ConvertFrom-Json
 Write-Output "Backup job details: $backupJobJson"
 
-
 # Polling for the completion of the backup job
 $backupJobId = $backupJob.Id
 Write-Output "Waiting for backup job to complete. ${backupJobId}"
@@ -145,18 +138,23 @@ do {
     Write-Output "Waiting for backup job to complete. Current status: $backupJobStatus"
 } while ($backupJobStatus -eq "InProgress")
 
+$containerName = $backupJob.properties.containerName
 
-# Original disk name
-$originalDiskName = "$vmName-$version-disk"
+$recoveryPointsJson = az backup recoverypoint list `
+    --container-name $containerName `
+    --item-name $vmName `
+    --resource-group $resourceGroupName `
+    --vault-name $vaultName `
+    --output json
 
-# Clean the disk name by removing invalid characters
-$cleanedDiskName = $originalDiskName -replace '[^A-Za-z0-9-]', ''
+#Convert JSON to PowerShell Object: Convert the JSON output to a PowerShell object for easier handling.
+$recoveryPoints = $recoveryPointsJson | ConvertFrom-Json
 
-# Generate 16 random lowercase letters
-$randomSuffix = -join ((97..122) | Get-Random -Count 16 | % {[char]$_})
+#Sort and Select the Latest Recovery Point: Sort the recovery points by their timestamp in descending order and select the first one, which is the most recent.
+$latestRecoveryPoint = $recoveryPoints | Sort-Object { $_.properties.recoveryPointTime } -Descending | Select-Object -First 1
 
-# Combine cleaned disk name with the random suffix
-$diskName = $cleanedDiskName + $randomSuffix
+#Extract the Recovery Point Name: Extract the name of the latest recovery point for use in your restore command.
+$latestRecoveryPointName = $latestRecoveryPoint.name
 
 # Restore VM to create a new managed disk
 Write-Output "Restoring disks for ${vmName}:"
@@ -164,20 +162,18 @@ Write-Output " --resource-group ${resourceGroupName}"
 Write-Output " --target-resource-group ${resourceGroupName}"
 Write-Output " --vault-name ${vaultName}"
 Write-Output " --storage-account ${storageAccountName}"
-Write-Output " --container-name ${backupJob.properties.containerName}"
-Write-Output " --item-name ${backupJob.properties.friendlyName}"
-Write-Output "--rp-name  ${backupJob.properties.entityFriendlyName}"
-Write-Output "--disk-name $diskName"
+Write-Output " --container-name $( $backupJob.properties.containerName )"
+Write-Output " --item-name $( $backupJob.properties.entityFriendlyName )"
+Write-Output "--rp-name  $( $backupJob.properties.entityFriendlyName )"
 
 $restoreJobJson = az backup restore restore-disks `
     --resource-group $resourceGroupName `
     --target-resource-group $resourceGroupName `
     --vault-name $vaultName `
     --storage-account $storageAccountName `
-    --container-name $backupJob.properties.containerName `
-    --item-name $backupJob.properties.entityFriendlyName `
-    --rp-name  $backupJob.properties.entityFriendlyName `
-    --disk-name $diskName `
+    --container-name $containerName `
+    --item-name $vmName `
+    --rp-name  $latestRecoveryPointName `
     --output json
 
 # Convert JSON string to PowerShell object
@@ -192,6 +188,29 @@ do {
     $restoreJobStatus = az backup job show --id $restoreJobId --query "properties.status" -o tsv
     Write-Output "Waiting for restore job to complete. Current status: $restoreJobStatus"
 } while ($restoreJobStatus -eq "InProgress" -or $restoreJobStatus -eq "InProgress")
+
+# Get the list of disks in JSON format
+$disksJson = az disk list --resource-group $resourceGroupName --output json
+
+# Convert JSON string to PowerShell object
+$disks = $disksJson | ConvertFrom-Json
+
+# Filter to find disks created by a restore operation
+$restoredDisks = $disks | Where-Object { $_.creationData.createOption -eq "Restore" }
+
+# Sort the restored disks by their creation time and select the latest one
+$latestRestoredDisk = $restoredDisks | Sort-Object { $_.timeCreated } -Descending | Select-Object -First 1
+
+# Output the name of the latest restored disk
+if ($latestRestoredDisk) {
+    Write-Output "Latest restored disk name: $( $latestRestoredDisk.name )"
+}
+else {
+    Write-Output "No restored disks found."
+}
+
+# Make the diskname a variable for easier use
+$diskName = $latestRestoredDisk.name
 
 # Take a snapshot of the created disk
 $snapshotName = "$vmName-$version"
