68e8e2b5081c9a0e410c23f0edc81a1437eff24f
Clint Baxley <clint.baxley-ctr@ecstech.com>
2023-12-27 05:41:28 -0500

    Fix the wec server name setting

--- testing/configure/wec_gpo_update_server_name.ps1
+++ testing/configure/wec_gpo_update_server_name.ps1
@@ -1,22 +1,9 @@
-# Query the registry to find the key path
-$HKEY_USERSKeys = reg query HKEY_USERS /s /f "SubscriptionManager" /k
+# To set the GP registry value
+Set-GPRegistryValue -Name "LME-WEC-Client" -Key "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\EventLog\EventForwarding\SubscriptionManager" -Value "Server=https://dc1.lme.local:5986/wsman/SubscriptionManager/WEC,Refresh=60" -Type String
 
-# Extract the first key path that contains "EventForwarding"
-$HKEY_USERSKeyPath = ($HKEY_USERSKeys -split "\r\n" | Where-Object { $_ -match "HKEY_USERS\\.*EventForwarding" }) -replace "HKEY_USERS\\", "HKU\" | Select-Object -First 1
+# To get the GP registry value to confirm it's set
+$registryValue = Get-GPRegistryValue -Name "LME-WEC-Client" -Key "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\EventLog\EventForwarding\SubscriptionManager"
 
-# Ensure the key path is not null or empty
-if (-not [string]::IsNullOrWhiteSpace($HKEY_USERSKeyPath)) {
-    # Display the current value
-    $oldValue = reg query "$HKEY_USERSKeyPath" /v "1"
-    Write-Host "Old Value: $oldValue"
-
-    # Change the SubscriptionManager key value
-    $newData = "Server=http://dc1.lme.local:5985/wsman/SubscriptionManager/WEC,Refresh=60"
-    reg add "$HKEY_USERSKeyPath" /v "1" /t REG_SZ /d "$newData" /f
-
-    # Requery to confirm the change
-    $newValue = reg query "$HKEY_USERSKeyPath" /v "1"
-    Write-Host "New Value: $newValue"
-} else {
-    Write-Error "Registry key path not found."
-}
+# Output the retrieved registry value
+Write-Host "Set the subscription manager url value to: "
+$registryValue
