ec19b8dbfc03aa796a93f843b18498b206bd3248
Clint Baxley <clint.baxley-ctr@ecstech.com>
2023-12-29 08:00:44 -0500

    Adds winlogbeat installer

--- testing/configure/linux_install_lme.sh
+++ testing/configure/linux_install_lme.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Change to the directory where the script is located
+cd "$(dirname "$0")" || exit 1
+
 # Default username
 username="admin.ackbar"
 
@@ -16,6 +19,5 @@ sudo git clone https://github.com/cisagov/lme.git /opt/lme/
 # Execute script with root privileges
 sudo ./linux_install_lme.exp
 
-# Replace 'admin.ackbar' with the specified username
 sudo cp /opt/lme/files_for_windows.zip /home/"$username"/
 sudo chown "$username":"$username" /home/"$username"/files_for_windows.zip
