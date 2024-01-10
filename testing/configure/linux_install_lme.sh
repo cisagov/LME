89552ab93506969a0d4152b815b86635a953600f
Clint Baxley <clint.baxley-ctr@ecstech.com>
2024-01-10 06:28:23 -0500

    Handle the reboot message for linux expect script

--- testing/configure/linux_install_lme.sh
+++ testing/configure/linux_install_lme.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Change to the directory where the script is located
-cd "$(dirname "$0")" || exit 1
+script_dir=$(dirname "$0")
+cd $script_dir || exit 1
+# We need to get the full path of the script dir for below
+script_dir=$(pwd)
 
 # Default username
 username="admin.ackbar"
@@ -16,13 +19,12 @@ done
 
 # Download a copy of the LME files
 sudo git clone https://github.com/cisagov/lme.git /opt/lme/
-
 # Execute script with root privileges
-sudo ./linux_install_lme.exp
+sudo "$script_dir/linux_install_lme.exp"
 
 if [ -f "/opt/lme/files_for_windows.zip" ]; then
     sudo cp /opt/lme/files_for_windows.zip /home/"$username"/
     sudo chown "$username":"$username" /home/"$username"/files_for_windows.zip
 else
-    echo "Warn: files_for_windows.zip does not exist. Probably because the LME install requires a reboot."
+    echo "files_for_windows.zip does not exist. Probably because the LME install requires a reboot."
 fi
