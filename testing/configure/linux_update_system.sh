9cb4dfcd493a4f2535f06d90f45fdfaa59cad6ce
Clint Baxley <clint.baxley-ctr@ecstech.com>
2024-01-29 05:18:17 -0500

    Up the timeout of the adding ls1.lme.local

--- testing/configure/linux_update_system.sh
+++ testing/configure/linux_update_system.sh
@@ -3,4 +3,4 @@ curl -sL https://deb.nodesource.com/setup_20.x -o nodesource_setup.sh
 chmod +x nodesource_setup.sh
 sudo ./nodesource_setup.sh
 sudo apt update
-sudo DEBIAN_FRONTEND=noninteractive NEEDRESTART_MODE=a apt install git curl zip net-tools jq nodejs expect -y
+sudo DEBIAN_FRONTEND=noninteractive NEEDRESTART_MODE=a apt install git curl zip net-tools jq nodejs expect python3-venv -y
