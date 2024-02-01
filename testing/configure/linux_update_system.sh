184f6a4881d00580715ed8b0f206491f95292532
Clint Baxley <clint.baxley-ctr@ecstech.com>
2024-02-01 05:12:39 -0500

    Remove node installer

--- testing/configure/linux_update_system.sh
+++ testing/configure/linux_update_system.sh
@@ -1,6 +1,3 @@
 # Install Git client to be able to clone the LME repository
-curl -sL https://deb.nodesource.com/setup_20.x -o nodesource_setup.sh
-chmod +x nodesource_setup.sh
-sudo ./nodesource_setup.sh
 sudo apt update
 sudo DEBIAN_FRONTEND=noninteractive NEEDRESTART_MODE=a apt install git curl zip net-tools jq nodejs expect python3-venv -y
