b748475fe3bf35ed4f70f39ea162f539513a64fe
unknown <admin.ackbar@LME.LOCAL>
2024-02-07 02:15:30 +0000

    Completed all dashboards. Requires testing now

--- testing/selenium_tests.py
+++ testing/selenium_tests.py
@@ -276,6 +276,333 @@ class UserHRTests(unittest.TestCase):
         panel = load_panel("All User Events by Day of Week, Hour of Day")
         self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
 
+    def test_timestamps_by_count(self):
+        """Is there any data?"""
+        panel = load_panel("Timestamps by Count")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_user_logon_events(self):
+        """Is there any data?"""
+        panel = load_panel("User logon events (filter by LogonId)")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_user_logoff_events(self):
+        """Is there any data?"""
+        panel = load_panel("User logoff events (correlate to logon events)")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_inperson_vs_remote_logons(self):
+        """Is there any data?"""
+        panel = load_panel("In person vs Remote logons")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+class SecurityDashboardSecurityLogTests(unittest.TestCase):
+    """Test cases for the Security Dashboard - Security Log Dashboard"""
+
+    def setUp(self):
+        # The dashboard ID is hard-coded in the ndjson file
+        dashboard_id = "51186cd0-e8e9-11e9-9070-f78ae052729a"
+        driver.get(f"https://ls1/app/dashboards#/view/{dashboard_id}")
+        expected_cond = EC.presence_of_element_located((By.CLASS_NAME, "react-grid-layout"))
+        WebDriverWait(driver, args.timeout).until(expected_cond)
+
+    def test_dashboard_menu(self):
+        """Is there any data?"""
+        panel = load_panel("Dashboard Menu")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_security_log_events(self):
+        """Is there any data?"""
+        panel = load_panel("Security logs events")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_computer_filter_results(self):
+        """Is there any data?"""
+        panel = load_panel("Select a computer to filter the below results. Leave blank for all")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_computer_filter(self):
+        """Is there any data?"""
+        panel = load_panel("Select a computername to filter")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_failed_logon_attempts(self):
+        """Is there any data?"""
+        panel = load_panel("Failed logon attempts")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_computers_showing_failed_login_attempts(self):
+        """Is there any data?"""
+        panel = load_panel("Computers showing failed login attempts - 10 maximum shown")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_failed_logons_type_codes(self):
+        """Is there any data?"""
+        panel = load_panel("Failed logon type codes")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_failed_logon_and_reason(self):
+        """Is there any data?"""
+        panel = load_panel("Failed logon and reason (status code)")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_failed_logons(self):
+        """Is there any data?"""
+        panel = load_panel("Failed Logons")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_failed_logon_status_codes(self):
+        """Is there any data?"""
+        panel = load_panel("Failed logon status codes")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_log_cleared_event_id_1102_or_104(self):
+        """Is there any data?"""
+        panel = load_panel("Log Cleared - event ID 1102 or 104")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_security_log_events_detail(self):
+        """Is there any data?"""
+        panel = load_panel("Security log events - Detail")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_security_log_process_creation_event_id_4688(self):
+        """Is there any data?"""
+        panel = load_panel("Security log - Process creation - event ID 4688")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_security_log_logon_created_logon_type_2(self):
+        """Is there any data?"""
+        panel = load_panel("Security log - Logon created - Logon type 2")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_security_log_network_logon_created_type_3(self):
+        """Is there any data?"""
+        panel = load_panel("Security log - network logon created - Logon type 3")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_security_log_logon_as_a_service_type_5(self):
+        """Is there any data?"""
+        panel = load_panel("Sercurity log - logon as a service - Logon type 5")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_credential_sent_as_clear_text_type_8(self):
+        """Is there any data?"""
+        panel = load_panel("Security log - Credential sent as clear text - Logon type 8")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_logons_with_special_privileges(self):
+        """Is there any data?"""
+        panel = load_panel("Security log - Logons with special privileges assigned - event ID 4672")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_process_started_with_different_creds(self):
+        """Is there any data?"""
+        panel = load_panel("Security log - Process started with different credentials- event ID 4648 [could be RUNAS, scheduled tasks]")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+class ComputerSoftwareOverviewTests(unittest.TestCase):
+    """Test cases for the Computer Software Overview Dashboard"""
+
+    def setUp(self):
+        # The dashboard ID is hard-coded in the ndjson file
+        dashboard_id = "33f0d3b0-8b8a-11ea-b1c6-a5bf39283f12"
+        driver.get(f"https://ls1/app/dashboards#/view/{dashboard_id}")
+        expected_cond = EC.presence_of_element_located((By.CLASS_NAME, "react-grid-layout"))
+        WebDriverWait(driver, args.timeout).until(expected_cond)
+
+    def test_dashboard_menu(self):
+        """Is there any data?"""
+        panel = load_panel("Dashboard Menu")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_host_count(self):
+        """Is there any data?"""
+        panel = load_panel("Host Count")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_filter_hosts(self):
+        """Is there any data?"""
+        panel = load_panel("Filter Hosts")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_processes(self):
+        """Is there any data?"""
+        panel = load_panel("Processes")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_application_crashing_and_hanging(self):
+        """Is there any data?"""
+        panel = load_panel("Application Crashing and Hanging")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_application_crashing_and_hanging_count(self):
+        """Is there any data?"""
+        panel = load_panel("Application Crashing and Hanging Count")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_create_remote_threat_events(self):
+        """Is there any data?"""
+        panel = load_panel("CreateRemoteThread events")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+class SysmonSummaryTests(unittest.TestCase):
+    """Test cases for the Sysmon Summary Dashboard"""
+
+    def setUp(self):
+        # The dashboard ID is hard-coded in the ndjson file
+        dashboard_id = "d2c73990-e5d4-11e9-8f1d-73a2ea4cc3ed"
+        driver.get(f"https://ls1/app/dashboards#/view/{dashboard_id}")
+        expected_cond = EC.presence_of_element_located((By.CLASS_NAME, "react-grid-layout"))
+        WebDriverWait(driver, args.timeout).until(expected_cond)
+
+    def test_total_number_of_sysmon_events_found(self):
+        """Is there any data?"""
+        panel = load_panel("Total number of Sysmon events found")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_percentage_of_sysmon_events_by_event_code(self):
+        """Is there any data?"""
+        panel = load_panel("Percentage of Sysmon events by event code")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_count_of_sysmon_events_by_event_code(self):
+        """Is there any data?"""
+        panel = load_panel("Count of Sysmon events by event code")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_top10_hosts_generating_most_sysmon_data(self):
+        """Is there any data?"""
+        panel = load_panel("Top 10 hosts generating the most Sysmon data")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_sysmon_event_code_reference(self):
+        """Is there any data?"""
+        panel = load_panel("Sysmon event code reference")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_sysmon_events(self):
+        """Is there any data?"""
+        panel = load_panel("Sysmon events")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+class ProcessExplorerTests(unittest.TestCase):
+    """Test cases for the Process Explorer Dashboard"""
+
+    def setUp(self):
+        # The dashboard ID is hard-coded in the ndjson file
+        dashboard_id = "f2cbc110-8400-11ee-a3de-f1bc0525ad6c"
+        driver.get(f"https://ls1/app/dashboards#/view/{dashboard_id}")
+        expected_cond = EC.presence_of_element_located((By.CLASS_NAME, "react-grid-layout"))
+        WebDriverWait(driver, args.timeout).until(expected_cond)
+
+    def test_process_spawns_over_time(self):
+        """Is there any data?"""
+        panel = load_panel("Process spawns over time")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_hosts(self):
+        """Is there any data?"""
+        panel = load_panel("Hosts")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_users(self):
+        """Is there any data?"""
+        panel = load_panel("Users")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_processes_created_by_users_over_time(self):
+        """Is there any data?"""
+        panel = load_panel("Processes created by users over time")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_process_spawn_event_logs_id1(self):
+        """Is there any data?"""
+        panel = load_panel("Process spawn event logs (Sysmon ID 1)")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_files_created_in_downloads(self):
+        """Is there any data?"""
+        panel = load_panel("Files created (in Downloads)")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_files_created_over_time_in_downloads(self):
+        """Is there any data?"""
+        panel = load_panel("Files created over time (in Downloads)")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_registry_events_sysmon_12_13_14(self):
+        """Is there any data?"""
+        panel = load_panel("Registry events (Sysmon 12, 13, 14)")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+class AlertingTests(unittest.TestCase):
+    """Test cases for the Alerting Dashboard"""
+    #Note, this dashboard from main on 2/6/2024 lacks panel titles.These tests may need to be fixed to accomodate. This dashboard may need updating in general to fit the other dashboards. It also lacks a dashboard menu
+
+    def setUp(self):
+        # The dashboard ID is hard-coded in the ndjson file
+        dashboard_id = "ac1078e0-8a32-11ea-8939-89f508ff7909"
+        driver.get(f"https://ls1/app/dashboards#/view/{dashboard_id}")
+        expected_cond = EC.presence_of_element_located((By.CLASS_NAME, "react-grid-layout"))
+        WebDriverWait(driver, args.timeout).until(expected_cond)
+
+    def test_signals_overview(self):
+        """Is there any data?"""
+        panel = load_panel("Signals Overview")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_mitre_attack_technique(self):
+        """Is there any data?"""
+        panel = load_panel("MITRE ATT&CK Technique")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_signals_details(self):
+        """Is there any data?"""
+        panel = load_panel("Signals Details")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_full_event_logs(self):
+        """Is there any data?"""
+        panel = load_panel("Full Event Logs")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+class HealthCheckTests(unittest.TestCase):
+    """Test cases for the HealthCheck Dashboard"""
+    #2/6/2024, main branch on lme. The health check dashboard has an odd dashboard menu. This will likely need updating.
+
+    def setUp(self):
+        # The dashboard ID is hard-coded in the ndjson file
+        dashboard_id = "51fe1470-fa59-11e9-bf25-8f92ffa3e3ec"
+        driver.get(f"https://ls1/app/dashboards#/view/{dashboard_id}")
+        expected_cond = EC.presence_of_element_located((By.CLASS_NAME, "react-grid-layout"))
+        WebDriverWait(driver, args.timeout).until(expected_cond)
+
+    def test_total_hosts(self):
+        """Is there any data?"""
+        panel = load_panel("Alpha - Health Check - Total Hosts")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_users_seen(self):
+        """Is there any data?"""
+        panel = load_panel("Users seen")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_number_of_admins(self):
+        """Is there any data?"""
+        panel = load_panel("Alpha - Health Check - Number of Admins - Metric (converted)")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_events_by_machine(self):
+        """Is there any data?"""
+        panel = load_panel("Events by machine")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
+    def test_unexpected_shutdowns(self):
+        """Is there any data?"""
+        panel = load_panel("Unexpected shutdowns")
+        self.assertFalse("No results found" in panel.get_attribute("innerHTML"))
+
 options = webdriver.ChromeOptions()
 # options.add_argument("--headless=new")
 options.add_experimental_option("detach", True) # Make browser stay open, in reality we'd
