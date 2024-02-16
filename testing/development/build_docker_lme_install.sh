77c71d811d3b1ff24ade172e3daf452f3f53b3bd
Clint <cbaxley@gmail.com>
2024-02-15 23:42:20 -0500

    Change the build path for building lme container

---
+++ testing/development/build_docker_lme_install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+git pull
+cd testing/configure || exit
+sudo ./linux_update_system.sh
+sudo ./linux_install_lme.sh -b cbaxley-168-python_tests
+. lib/functions.sh
+extract_credentials
+cd ../tests/ || exit
+pip install -r requirements.txt
+#pytest api_tests/linux_only/
