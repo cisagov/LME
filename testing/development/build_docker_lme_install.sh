d7067f16d491a022cee276a2a0a0812dd8dca4f3
cbaxley <cbaxley@gmail.com>
2024-02-16 08:31:22 -0500

    Set up the build for tests

--- testing/development/build_docker_lme_install.sh
+++ testing/development/build_docker_lme_install.sh
@@ -5,5 +5,7 @@ sudo ./linux_install_lme.sh -b cbaxley-168-python_tests
 . lib/functions.sh
 extract_credentials
 cd ../tests/ || exit
+python3 -m venv ~/venv_test
+. ~/venv_test/bin/activate
 pip install -r requirements.txt
 #pytest api_tests/linux_only/
