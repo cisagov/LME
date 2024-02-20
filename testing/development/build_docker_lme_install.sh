5ca55558655504e75678944080f3b25d564664f4
cbaxley <cbaxley@gmail.com>
2024-02-20 10:03:34 -0500

    Reduce logging for docker pull.

--- testing/development/build_docker_lme_install.sh
+++ testing/development/build_docker_lme_install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd testing/configure || exit
 sudo ./linux_update_system.sh
-sudo ./linux_install_lme.sh -b cbaxley-168-python_tests
+sudo ./linux_install_lme.sh -b cbaxley-194-workflows
 . lib/functions.sh
 extract_credentials
 cd ../tests/ || exit
