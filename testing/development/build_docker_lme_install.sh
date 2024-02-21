1ad6210bdfd0eca498fd5c173f760853a905fddb
cbaxley <cbaxley@gmail.com>
2024-02-21 09:13:12 -0500

    Adjust scripts to run as a non super user

--- testing/development/build_docker_lme_install.sh
+++ testing/development/build_docker_lme_install.sh
@@ -5,7 +5,8 @@ sudo ./linux_install_lme.sh -b cbaxley-194-workflows
 . lib/functions.sh
 extract_credentials
 cd ../tests/ || exit
-python3 -m venv ~/venv_test
-. ~/venv_test/bin/activate
+python3 -m venv /home/admin.ackbar/venv_test
+. /home/admin.ackbar/venv_test/bin/activate
 pip install -r requirements.txt
+chown admin.ackbar:admin.ackbar /home/admin.ackbar/venv_test -R
 #pytest api_tests/linux_only/
