5030946aa62cfebf0999296938262aa40b0c07ea
cbaxley <cbaxley@gmail.com>
2024-02-28 07:59:18 -0500

    Build a cluster using the generated resource group

--- testing/development/build_cluster.ps1
+++ testing/development/build_cluster.ps1
@@ -1,5 +1,4 @@
 $ErrorActionPreference = 'Stop'
 az login --service-principal -u $env:AZURE_CLIENT_ID -p $env:AZURE_SECRET --tenant $env:AZURE_TENANT
 cd testing
-echo $env:RESOURCE_GROUP
-# ./SetupTestbed.ps1 -AllowedSources "73.84.196.126/32" -l centralus -ResourceGroup $env:RESOURCE_GROUP -m  -y
+./SetupTestbed.ps1 -AllowedSources "73.84.196.126/32" -l centralus -ResourceGroup $env:RESOURCE_GROUP -m  -y
