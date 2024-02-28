13a23a64ac665cb1d2b6630904a0cf3bea7b6ad7
cbaxley <cbaxley@gmail.com>
2024-02-28 07:42:42 -0500

    Try to build a cluster with the build_cluster.ps1 script.

--- testing/development/build_cluster.ps1
+++ testing/development/build_cluster.ps1
@@ -1,4 +1,4 @@
 $ErrorActionPreference = 'Stop'
 az login --service-principal -u $env:AZURE_CLIENT_ID -p $env:AZURE_SECRET --tenant $env:AZURE_TENANT
 cd testing
-./SetupTestbed.ps1
+./SetupTestbed.ps1 -AllowedSources "73.84.196.126/32" -l centralus -ResourceGroup LME-$env:UNIQUE_ID -m  -y
