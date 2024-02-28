5d76be279c26946a3deffe6f3293371dbd249652
cbaxley <cbaxley@gmail.com>
2024-02-28 08:05:46 -0500

    Make the paths relative in the build_cluster script

--- testing/development/build_cluster.ps1
+++ testing/development/build_cluster.ps1
@@ -1,4 +1,13 @@
 $ErrorActionPreference = 'Stop'
+
+# Log in using Azure CLI
 az login --service-principal -u $env:AZURE_CLIENT_ID -p $env:AZURE_SECRET --tenant $env:AZURE_TENANT
-cd testing
-./SetupTestbed.ps1 -AllowedSources "73.84.196.126/32" -l centralus -ResourceGroup $env:RESOURCE_GROUP -m  -y
+
+# Construct the path to the target directory relative to the script's location
+$targetDirectory = Join-Path -Path $PSScriptRoot -ChildPath "..\testing"
+
+# Change to the target directory
+Set-Location -Path $targetDirectory
+
+# Execute the SetupTestbed.ps1 script with parameters
+.\SetupTestbed.ps1 -AllowedSources "73.84.196.126/32" -l centralus -ResourceGroup $env:RESOURCE_GROUP -m -y
