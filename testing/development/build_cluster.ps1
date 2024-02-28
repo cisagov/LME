0ee69757f7a6f816679977d2050becb632e4c6a9
cbaxley <cbaxley@gmail.com>
2024-02-28 06:19:59 -0500

    Send lastexitcode

--- testing/development/build_cluster.ps1
+++ testing/development/build_cluster.ps1
@@ -1,14 +1,6 @@
-function Run-ExternalCommand {
-    param(
-        [Parameter(Mandatory=$true)]
-        [string]$Command
-    )
-    & cmd /c $Command
-    if ($LASTEXITCODE -ne 0) {
-        Write-Host "An error occurred running '$Command', exit code was $LASTEXITCODE"
-        exit $LASTEXITCODE
-    }
+$ErrorActionPreference = 'Stop'
+az login --service-principal -u $AZURE_CLIENT_ID -p $env:AZURE_SECRET --tenant $env:AZURE_TENANT
+if ($LASTEXITCODE -ne 0) {
+    Write-Host "Login failed with exit code $LASTEXITCODE"
+    exit $LASTEXITCODE
 }
-
-# Now use this function to run external commands
-Run-ExternalCommand -Command "az login --service-principal -u `"$AZURE_CLIENT_ID`" -p `"$env:AZURE_SECRET`" --tenant `"$env:AZURE_TENANT`""
