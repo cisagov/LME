cd61f60e5e3cc31b9b93f27a234ebc53dbbecf23
cbaxley <cbaxley@gmail.com>
2024-02-28 08:10:14 -0500

    Move to the right directory to do an install

--- testing/development/build_cluster.ps1
+++ testing/development/build_cluster.ps1
@@ -4,7 +4,7 @@ $ErrorActionPreference = 'Stop'
 az login --service-principal -u $env:AZURE_CLIENT_ID -p $env:AZURE_SECRET --tenant $env:AZURE_TENANT
 
 # Construct the path to the target directory relative to the script's location
-$targetDirectory = Join-Path -Path $PSScriptRoot -ChildPath "..\testing"
+$targetDirectory = Join-Path -Path $PSScriptRoot -ChildPath "..\"
 
 # Change to the target directory
 Set-Location -Path $targetDirectory
