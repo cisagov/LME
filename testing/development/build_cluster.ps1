3e36eec65cadd4357c3911d623aad19fd5547c4e
cbaxley <cbaxley@gmail.com>
2024-02-28 07:32:24 -0500

    Add a passing command for build_cluster.ps1

--- testing/development/build_cluster.ps1
+++ testing/development/build_cluster.ps1
@@ -1,6 +1,2 @@
 $ErrorActionPreference = 'Stop'
-az login --service-principal -u $AZURE_CLIENT_ID -p $env:AZURE_SECRET --tenant $env:AZURE_TENANT
-# if ($LASTEXITCODE -ne 0) {
-#     Write-Host "Login failed with exit code $LASTEXITCODE"
-#     exit $LASTEXITCODE
-# }
+az login --service-principal -u $env:AZURE_CLIENT_ID -p $env:AZURE_SECRET --tenant $env:AZURE_TENANT
