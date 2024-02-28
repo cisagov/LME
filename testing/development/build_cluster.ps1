b48397d5c1ddbfb5972ad4472e124a585a01a4cd
cbaxley <cbaxley@gmail.com>
2024-02-28 05:20:16 -0500

    Capture the output from the az commands

--- testing/development/build_cluster.ps1
+++ testing/development/build_cluster.ps1
@@ -1,1 +1,10 @@
-az login --service-principal -u  -p $env:AZURE_SECRET --tenant $env:AZURE_TENANT
+try {
+    $output = az login --service-principal -u  -p $env:AZURE_SECRET --tenant $env:AZURE_TENANT 2>&1
+    Write-Output $output
+} catch {
+    Write-Error $_
+    exit 1
+}
+
+
+
