7612e3c35b10ce55f6e6863a0e69df5907187496
cbaxley <cbaxley@gmail.com>
2024-02-28 06:11:40 -0500

    Try propigating errors from pwsh

--- testing/development/build_cluster.ps1
+++ testing/development/build_cluster.ps1
@@ -1,1 +1,7 @@
-az login --service-principal -u $AZURE_CLIENT_ID -p $env:AZURE_SECRET --tenant $env:AZURE_TENANT
+$ErrorActionPreference = 'Stop'
+try {
+    az login --service-principal -u $AZURE_CLIENT_ID -p $env:AZURE_SECRET --tenant $env:AZURE_TENANT
+} catch {
+    Write-Host "An error occurred: $_"
+    exit 1
+}
