18eb1826bd8dcb2a02bf2408917ed979b95d839a
cbaxley <cbaxley@gmail.com>
2024-02-28 06:17:44 -0500

    Capture external command exit code

--- testing/development/build_cluster.ps1
+++ testing/development/build_cluster.ps1
@@ -1,7 +1,14 @@
-$ErrorActionPreference = 'Stop'
-try {
-    az login --service-principal -u $AZURE_CLIENT_ID -p $env:AZURE_SECRET --tenant $env:AZURE_TENANT
-} catch {
-    Write-Host "An error occurred: $_"
-    exit 1
+function Run-ExternalCommand {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$Command
+    )
+    & cmd /c $Command
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host "An error occurred running '$Command', exit code was $LASTEXITCODE"
+        exit $LASTEXITCODE
+    }
 }
+
+# Now use this function to run external commands
+Run-ExternalCommand -Command "az login --service-principal -u `"$AZURE_CLIENT_ID`" -p `"$env:AZURE_SECRET`" --tenant `"$env:AZURE_TENANT`""
