60e5d812c4b67cd92efd8314990e0b73c7278cf4
cbaxley <cbaxley@gmail.com>
2024-02-28 07:54:07 -0500

    Check resource group variable

--- testing/development/build_cluster.ps1
+++ testing/development/build_cluster.ps1
@@ -1,4 +1,5 @@
 $ErrorActionPreference = 'Stop'
 az login --service-principal -u $env:AZURE_CLIENT_ID -p $env:AZURE_SECRET --tenant $env:AZURE_TENANT
 cd testing
-./SetupTestbed.ps1 -AllowedSources "73.84.196.126/32" -l centralus -ResourceGroup LME-$env:UNIQUE_ID -m  -y
+echo $env:RESOURCE_GROUP
+# ./SetupTestbed.ps1 -AllowedSources "73.84.196.126/32" -l centralus -ResourceGroup $env:RESOURCE_GROUP -m  -y
