c2eb1374e766d56dac8a25cf678de05f01d9ad53
cbaxley <cbaxley@gmail.com>
2024-02-28 07:31:15 -0500

    Remove extra exit from build_cluster.ps1

--- testing/development/build_cluster.ps1
+++ testing/development/build_cluster.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = 'Stop'
 az login --service-principal -u $AZURE_CLIENT_ID -p $env:AZURE_SECRET --tenant $env:AZURE_TENANT
-if ($LASTEXITCODE -ne 0) {
-    Write-Host "Login failed with exit code $LASTEXITCODE"
-    exit $LASTEXITCODE
-}
+# if ($LASTEXITCODE -ne 0) {
+#     Write-Host "Login failed with exit code $LASTEXITCODE"
+#     exit $LASTEXITCODE
+# }
