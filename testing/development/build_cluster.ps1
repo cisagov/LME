09de7a76ec46bd91c9f9b07ac6b809234691e2a7
cbaxley <cbaxley@gmail.com>
2024-03-04 09:02:11 -0500

    Comment out building cluster in steps

--- testing/development/build_cluster.ps1
+++ testing/development/build_cluster.ps1
@@ -10,4 +10,5 @@ $targetDirectory = Join-Path -Path $PSScriptRoot -ChildPath "..\"
 Set-Location -Path $targetDirectory
 
 # Execute the SetupTestbed.ps1 script with parameters
-.\SetupTestbed.ps1 -AllowedSources "73.84.196.126/32" -l centralus -ResourceGroup $env:RESOURCE_GROUP -y
+# TODO: Uncomment this before merging
+#.\SetupTestbed.ps1 -AllowedSources "73.84.196.126/32" -l centralus -ResourceGroup $env:RESOURCE_GROUP -y
