9cbb7b4e8f0df6996c7d6931272fbe2a4482d2ab
cbaxley <cbaxley@gmail.com>
2024-03-05 14:09:35 -0500

    Install lme on the cluster

---
+++ testing/development/install_lme.ps1
@@ -0,0 +1,13 @@
+$ErrorActionPreference = 'Stop'
+
+# Log in using Azure CLI
+az login --service-principal -u $env:AZURE_CLIENT_ID -p $env:AZURE_SECRET --tenant $env:AZURE_TENANT
+
+# Construct the path to the target directory relative to the script's location
+$targetDirectory = Join-Path -Path $PSScriptRoot -ChildPath "..\\"
+
+# Change to the target directory
+Set-Location -Path $targetDirectory
+
+# Execute the InstallTestbed.ps1 script with parameters
+.\InstallTestbed.ps1 -ResourceGroup $env:RESOURCE_GROUP  -m | Tee-Object -FilePath "./$env:RESOURCE_GROUP.output.log"
