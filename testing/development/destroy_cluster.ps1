74aca821570338a19ba9ff4d6c987caf7ec71670
cbaxley <cbaxley@gmail.com>
2024-03-11 07:39:53 -0400

    Don't destroy cluster so we can manually test

--- testing/development/destroy_cluster.ps1
+++ testing/development/destroy_cluster.ps1
@@ -11,7 +11,7 @@ $resourceGroupExists = az group exists --name "$env:RESOURCE_GROUP"
 
 if ($resourceGroupExists -eq 'true') {
     # Delete the resource group if it exists
-    az group delete --name "$env:RESOURCE_GROUP" --yes --no-wait
+    # az group delete --name "$env:RESOURCE_GROUP" --yes --no-wait
     Write-Host "Deletion of resource group $($env:RESOURCE_GROUP) initiated."
 } else {
     Write-Host "Resource group $($env:RESOURCE_GROUP) does not exist. No action taken."
