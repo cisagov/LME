599b0446fb2c09f11e20f93cb0e26c55aec66780
cbaxley <cbaxley@gmail.com>
2024-03-20 05:38:57 -0400

    Build lme container fresh before install

--- testing/development/build_docker_lme_install.sh
+++ testing/development/build_docker_lme_install.sh
@@ -5,6 +5,7 @@ sudo ./linux_update_system.sh
 sudo ./linux_install_lme.sh -b cbaxley-194-workflows
 . lib/functions.sh
 extract_credentials
+echo $elastic
 cd ../tests/ || exit
 python3 -m venv /home/admin.ackbar/venv_test
 . /home/admin.ackbar/venv_test/bin/activate
