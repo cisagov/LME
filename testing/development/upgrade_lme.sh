2f0d8177e9b2bf1f9990884b23f5f32a0d3b49d1
cbaxley <cbaxley@gmail.com>
2024-04-02 09:25:03 -0400

    Capture the output of the upgrade script to fail pipeline if it fails

--- testing/development/upgrade_lme.sh
+++ testing/development/upgrade_lme.sh
@@ -26,3 +26,5 @@ sudo  echo "Forcing version: $FORCE_LATEST_VERSION"
 sudo  sh -c "cd '/opt/lme/' && git checkout 'Chapter\ 3\ Files/deploy.sh' && git checkout -t origin/$current_branch && git pull"
 echo "Running the upgrade"
 sudo  sh -c "export TERM=dumb; export FORCE_LATEST_VERSION=$FORCE_LATEST_VERSION; cd '/opt/lme/Chapter 3 Files' && ./deploy.sh upgrade" 
+
+echo "UPGRADE_SUCCESSFUL"
