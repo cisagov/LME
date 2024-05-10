a415dd906b044399990cee0cff952fcd4b0d0c4c
Rishi <rishi.aggarwal@associates.cisa.dhs.gov>
2024-05-10 10:21:22 -0400

    Updated Selenium test scripts for Health Check Dashboard

--- testing/tests/selenium_tests/cluster/test_health_check_dashboard.py
+++ testing/tests/selenium_tests/cluster/test_health_check_dashboard.py
@@ -2,6 +2,9 @@ import pytest
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+from .lib import dashboard_test_function
 
 class TestHealthCheckDashboard:
     dashboard_id = "51fe1470-fa59-11e9-bf25-8f92ffa3e3ec"
@@ -13,51 +16,27 @@ class TestHealthCheckDashboard:
 
     def test_number_of_admins(self, setup_login, kibana_url, timeout):
         driver = setup_login
-        driver.get(f"{kibana_url}/app/dashboards#/view/{self.dashboard_id}")
-        expected_cond = EC.presence_of_element_located((By.CLASS_NAME, "react-grid-layout"))
-        WebDriverWait(driver, timeout).until(expected_cond)
-        panel_title = "Number of Admins"
-        selector = f'div[data-title="{panel_title}"]'
-        expected_cond = EC.presence_of_element_located((By.CSS_SELECTOR, selector))
-        WebDriverWait(driver, timeout).until(expected_cond)
-        panel = driver.find_element(By.CSS_SELECTOR, selector)
-        assert "No results found" not in panel.get_attribute("innerHTML")
+        dashboard_test_function(driver, kibana_url, timeout, self.dashboard_id, "Number of Admins", ".expExpressionRenderer",".dummyval")
+        # The arguement ".dummyval" is being used though it is not a valid selector. 
+        # This panel should always have a visualization so there should never be no data message displayed.
+        # If there is no visualization rendered or "No Results found" message is displayed for this panel on dashboard, this test should fail which is correct behavior
+        
 
     def test_total_hosts(self, setup_login, kibana_url, timeout):
         driver = setup_login
-        driver.get(f"{kibana_url}/app/dashboards#/view/{self.dashboard_id}")
-        expected_cond = EC.presence_of_element_located((By.CLASS_NAME, "react-grid-layout"))
-        WebDriverWait(driver, timeout).until(expected_cond)
-        panel_title = "Total Hosts"
-        selector = f'div[data-title="{panel_title}"]'
-        expected_cond = EC.presence_of_element_located((By.CSS_SELECTOR, selector))
-        WebDriverWait(driver, timeout).until(expected_cond)
-        panel = driver.find_element(By.CSS_SELECTOR, selector)
-        assert "No results found" not in panel.get_attribute("innerHTML")
+        dashboard_test_function(driver, kibana_url, timeout, self.dashboard_id, "Total Hosts", ".visualization",".dummyval")
 
     def test_events_by_machine(self, setup_login, kibana_url, timeout):
         driver = setup_login
-        driver.get(f"{kibana_url}/app/dashboards#/view/{self.dashboard_id}")
-        expected_cond = EC.presence_of_element_located((By.CLASS_NAME, "react-grid-layout"))
-        WebDriverWait(driver, timeout).until(expected_cond)
-        panel_title = "Events by machine"
-        selector = f'div[data-title="{panel_title}"]'
-        expected_cond = EC.presence_of_element_located((By.CSS_SELECTOR, selector))
-        WebDriverWait(driver, timeout).until(expected_cond)
-        panel = driver.find_element(By.CSS_SELECTOR, selector)
-        assert "No results found" not in panel.get_attribute("innerHTML")
+        dashboard_test_function(driver, kibana_url, timeout, self.dashboard_id, "Events by machine", ".echChart",".euiText")
 
     @pytest.mark.skip(reason="Skipping this test")
     def test_unexpected_shutdowns(self, setup_login, kibana_url, timeout):
         driver = setup_login
-        driver.get(f"{kibana_url}/app/dashboards#/view/{self.dashboard_id}")
-        expected_cond = EC.presence_of_element_located((By.CLASS_NAME, "react-grid-layout"))
-        WebDriverWait(driver, timeout).until(expected_cond)
-        panel_title = "Unexpected shutdowns"
-        selector = f'div[data-title="{panel_title}"]'
-        expected_cond = EC.presence_of_element_located((By.CSS_SELECTOR, selector))
-        WebDriverWait(driver, timeout).until(expected_cond)
-        panel = driver.find_element(By.CSS_SELECTOR, selector)
-        assert "No results found" not in panel.get_attribute("innerHTML")
+        dashboard_test_function(driver, kibana_url, timeout, self.dashboard_id, "Unexpected shutdowns", ".echChart",".visError")
+
+    def test_users_seen(self, setup_login, kibana_url, timeout):
+        driver = setup_login
+        dashboard_test_function(driver, kibana_url, timeout, self.dashboard_id, "Users seen", ".visualization",".dummyval")
 
 
