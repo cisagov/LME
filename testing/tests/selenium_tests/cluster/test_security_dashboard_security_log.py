5e0c8c3b855da695a34f0251a108e08629a3f3f0
Rishi <rishi.aggarwal@associates.cisa.dhs.gov>
2024-05-14 07:39:45 -0400

    Rewrite completed for Selenium test scripts for Security Dashboard - Security Log

--- testing/tests/selenium_tests/cluster/test_security_dashboard_security_log.py
+++ testing/tests/selenium_tests/cluster/test_security_dashboard_security_log.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+from .lib import dashboard_test_function
 
 class TestSecurityDashboardSecurityLog:
     dashboard_id = "51186cd0-e8e9-11e9-9070-f78ae052729a"
@@ -15,199 +16,83 @@ class TestSecurityDashboardSecurityLog:
 
     def test_computer_filter_results(self, setup_login, kibana_url, timeout):
         driver = setup_login
-        driver.get(f"{kibana_url}/app/dashboards#/view/{self.dashboard_id}")
-        expected_cond = EC.presence_of_element_located((By.CLASS_NAME, "react-grid-layout"))
-        WebDriverWait(driver, timeout).until(expected_cond)
-        panel_title = "Select a computer to filter the below results.  Leave blank for all"
-        selector = f'div[data-title="{panel_title}"]'
-        expected_cond = EC.presence_of_element_located((By.CSS_SELECTOR, selector))
-        WebDriverWait(driver, timeout).until(expected_cond)
-        panel = driver.find_element(By.CSS_SELECTOR, selector)
-        assert "No results found" not in panel.get_attribute("innerHTML")
+        dashboard_test_function(driver, kibana_url, timeout, self.dashboard_id, "Select a computer to filter the below results.  Leave blank for all", ".euiFlexGroup",".dummyval")
+        # The arguement ".dummyval" is being used though it is not a valid selector. 
+        # This panel should always have a visualization so there should never be no data message displayed.
+        # If there is no visualization rendered or "No Results found" message is displayed for this panel on dashboard, this test should fail which is correct behavior
 
+    
+    @pytest.mark.skip(reason="Skipping this test")
     def test_logons_with_special_privileges(self, setup_login, kibana_url, timeout):
+        #This dashboard panel needs test data. Currently the panel only gives No Result found
         driver = setup_login
-        driver.get(f"{kibana_url}/app/dashboards#/view/{self.dashboard_id}")
-        expected_cond = EC.presence_of_element_located((By.CLASS_NAME, "react-grid-layout"))
-        WebDriverWait(driver, timeout).until(expected_cond)
-        panel_title = "Security log -  Logons with special privileges assigned - event ID 4672"
-        selector = f'div[data-title="{panel_title}"]'
-        expected_cond = EC.presence_of_element_located((By.CSS_SELECTOR, selector))
-        WebDriverWait(driver, timeout).until(expected_cond)
-        panel = driver.find_element(By.CSS_SELECTOR, selector)
-        assert "No results found" not in panel.get_attribute("innerHTML")
-
+        dashboard_test_function(driver, kibana_url, timeout, self.dashboard_id, "Security log -  Logons with special privileges assigned - event ID 4672", ".needarealvaluehere",".visError")
+              
     def test_computer_filter(self, setup_login, kibana_url, timeout):
         driver = setup_login
-        driver.get(f"{kibana_url}/app/dashboards#/view/{self.dashboard_id}")
-        expected_cond = EC.presence_of_element_located((By.CLASS_NAME, "react-grid-layout"))
-        WebDriverWait(driver, timeout).until(expected_cond)
-        panel_title = "Select a computername to filter"
-        selector = f'div[data-title="{panel_title}"]'
-        expected_cond = EC.presence_of_element_located((By.CSS_SELECTOR, selector))
-        WebDriverWait(driver, timeout).until(expected_cond)
-        panel = driver.find_element(By.CSS_SELECTOR, selector)
-        assert "No results found" not in panel.get_attribute("innerHTML")
-
+        dashboard_test_function(driver, kibana_url, timeout, self.dashboard_id, "Select a computername to filter", ".tbvChart",".visError")
+        
     def test_computers_showing_failed_login_attempts_none(self, setup_login, kibana_url, timeout):
         driver = setup_login
-        driver.get(f"{kibana_url}/app/dashboards#/view/{self.dashboard_id}")
-
-        # Wait for the react-grid-layout element to be present
-        expected_cond = EC.presence_of_element_located((By.CLASS_NAME, "react-grid-layout"))
-        WebDriverWait(driver, timeout).until(expected_cond)
-
-        panel_title = "Computers showing failed login attempts - 10 maximum shown"
-        selector = f'div[data-title="{panel_title}"]'
-
-        # Wait for the specific panel to be present
-        expected_cond = EC.presence_of_element_located((By.CSS_SELECTOR, selector))
-        WebDriverWait(driver, timeout).until(expected_cond)
-
-        # Wait for either the panel content or the "No results found" message to be present
-        panel_content_selector = f"{selector} .echChart"
-        no_results_selector = f"{selector} .visError"
-        expected_cond = EC.presence_of_element_located((By.CSS_SELECTOR, f"{panel_content_selector}, {no_results_selector}"))
-        WebDriverWait(driver, timeout).until(expected_cond)
-
-        panel = driver.find_element(By.CSS_SELECTOR, selector)
-        
-        # Check if the panel content is present
-        try:
-            # Check if the "No results found" message is present
-            no_results_message = driver.find_element(By.CSS_SELECTOR, no_results_selector)
-            assert no_results_message.is_displayed()
-        except NoSuchElementException:
-            panel_content = driver.find_element(By.CSS_SELECTOR, panel_content_selector)
-            assert panel_content.is_displayed()
+        dashboard_test_function(driver, kibana_url, timeout, self.dashboard_id, "Computers showing failed login attempts - 10 maximum shown", ".echChart",".visError")
 
     @pytest.mark.skip(reason="Skipping this test")
     def test_credential_sent_as_clear_text_type_8(self, setup_login, kibana_url, timeout):
+        #This dashboard panel needs test data. Currently the panel only gives No Result found
         driver = setup_login
-        driver.get(f"{kibana_url}/app/dashboards#/view/{self.dashboard_id}")
-        expected_cond = EC.presence_of_element_located((By.CLASS_NAME, "react-grid-layout"))
-        WebDriverWait(driver, timeout).until(expected_cond)
-        panel_title = "Security log - Credential sent as clear text - Logon type 8"
-        selector = f'div[data-title="{panel_title}"]'
-        expected_cond = EC.presence_of_element_located((By.CSS_SELECTOR, selector))
-        WebDriverWait(driver, timeout).until(expected_cond)
-        panel = driver.find_element(By.CSS_SELECTOR, selector)
-        assert "No results found" not in panel.get_attribute("innerHTML")
+        dashboard_test_function(driver, kibana_url, timeout, self.dashboard_id, "Security log - Credential sent as clear text - Logon type 8", ".needarealvaluehere",".visError")
+   
 
-    @pytest.mark.skip(reason="Skipping this test")
     def test_failed_logon_and_reason(self, setup_login, kibana_url, timeout):
         driver = setup_login
-        driver.get(f"{kibana_url}/app/dashboards#/view/{self.dashboard_id}")
-        expected_cond = EC.presence_of_element_located((By.CLASS_NAME, "react-grid-layout"))
-        WebDriverWait(driver, timeout).until(expected_cond)
-        panel_title = "Failed logon and reason (status code)"
-        selector = f'div[data-title="{panel_title}"]'
-        expected_cond = EC.presence_of_element_located((By.CSS_SELECTOR, selector))
-        WebDriverWait(driver, timeout).until(expected_cond)
-        panel = driver.find_element(By.CSS_SELECTOR, selector)
-        assert "No results found" not in panel.get_attribute("innerHTML")
-
-    @pytest.mark.skip(reason="Skipping this test")
+        dashboard_test_function(driver, kibana_url, timeout, self.dashboard_id, "Failed logon and reason (status code)", ".echChart",".euiText")
+        
     def test_failed_logons(self, setup_login, kibana_url, timeout):
         driver = setup_login
-        driver.get(f"{kibana_url}/app/dashboards#/view/{self.dashboard_id}")
-        expected_cond = EC.presence_of_element_located((By.CLASS_NAME, "react-grid-layout"))
-        WebDriverWait(driver, timeout).until(expected_cond)
-        panel_title = "Failed Logons"
-        selector = f'div[data-title="{panel_title}"]'
-        expected_cond = EC.presence_of_element_located((By.CSS_SELECTOR, selector))
-        WebDriverWait(driver, timeout).until(expected_cond)
-        panel = driver.find_element(By.CSS_SELECTOR, selector)
-        assert "No results found" not in panel.get_attribute("innerHTML")
-
+        dashboard_test_function(driver, kibana_url, timeout, self.dashboard_id, "Failed Logons", ".unifiedDataTable",".euiDataGrid__noResults")
+        
     @pytest.mark.skip(reason="Skipping this test")
     def test_log_cleared_event_id_1102_or_104(self, setup_login, kibana_url, timeout):
+        #This dashboard panel needs test data. Currently the panel only gives No Result found
         driver = setup_login
-        driver.get(f"{kibana_url}/app/dashboards#/view/{self.dashboard_id}")
-        expected_cond = EC.presence_of_element_located((By.CLASS_NAME, "react-grid-layout"))
-        WebDriverWait(driver, timeout).until(expected_cond)
-        panel_title = "Log Cleared - event ID 1102 or 104"
-        selector = f'div[data-title="{panel_title}"]'
-        expected_cond = EC.presence_of_element_located((By.CSS_SELECTOR, selector))
-        WebDriverWait(driver, timeout).until(expected_cond)
-        panel = driver.find_element(By.CSS_SELECTOR, selector)
-        assert "No results found" not in panel.get_attribute("innerHTML")
+        dashboard_test_function(driver, kibana_url, timeout, self.dashboard_id, "Log Cleared - event ID 1102 or 104", ".needarealvaluehere",".euiDataGrid__noResults")
 
-    @pytest.mark.skip(reason="Skipping this test")
+
     def test_process_started_with_different_creds(self, setup_login, kibana_url, timeout):
         driver = setup_login
-        driver.get(f"{kibana_url}/app/dashboards#/view/{self.dashboard_id}")
-        expected_cond = EC.presence_of_element_located((By.CLASS_NAME, "react-grid-layout"))
-        WebDriverWait(driver, timeout).until(expected_cond)
-        panel_title = "Security log - Process started with different credentials- event ID 4648 [could be RUNAS, scheduled tasks]"
-        selector = f'div[data-title="{panel_title}"]'
-        expected_cond = EC.presence_of_element_located((By.CSS_SELECTOR, selector))
-        WebDriverWait(driver, timeout).until(expected_cond)
-        panel = driver.find_element(By.CSS_SELECTOR, selector)
-        assert "No results found" not in panel.get_attribute("innerHTML")
-
+        dashboard_test_function(driver, kibana_url, timeout, self.dashboard_id, "Security log - Process started with different credentials- event ID 4648 [could be RUNAS, scheduled tasks]", ".euiDataGrid",".euiDataGrid__noResults")
+        
     def test_security_log_events_detail(self, setup_login, kibana_url, timeout):
         driver = setup_login
-        driver.get(f"{kibana_url}/app/dashboards#/view/{self.dashboard_id}")
-        expected_cond = EC.presence_of_element_located((By.CLASS_NAME, "react-grid-layout"))
-        WebDriverWait(driver, timeout).until(expected_cond)
-        panel_title = "Security log events - Detail"
-        selector = f'div[data-title="{panel_title}"]'
-        expected_cond = EC.presence_of_element_located((By.CSS_SELECTOR, selector))
-        WebDriverWait(driver, timeout).until(expected_cond)
-        panel = driver.find_element(By.CSS_SELECTOR, selector)
-        assert "No results found" not in panel.get_attribute("innerHTML")
-
-    @pytest.mark.skip(reason="Skipping this test")
+        dashboard_test_function(driver, kibana_url, timeout, self.dashboard_id, "Security log events - Detail", ".euiDataGrid",".euiDataGrid__noResults")
+        
     def test_security_log_logon_as_a_service_type_5(self, setup_login, kibana_url, timeout):
         driver = setup_login
-        driver.get(f"{kibana_url}/app/dashboards#/view/{self.dashboard_id}")
-        expected_cond = EC.presence_of_element_located((By.CLASS_NAME, "react-grid-layout"))
-        WebDriverWait(driver, timeout).until(expected_cond)
-        panel_title = "Sercurity log - logon as a service - Logon type 5"
-        selector = f'div[data-title="{panel_title}"]'
-        expected_cond = EC.presence_of_element_located((By.CSS_SELECTOR, selector))
-        WebDriverWait(driver, timeout).until(expected_cond)
-        panel = driver.find_element(By.CSS_SELECTOR, selector)
-        assert "No results found" not in panel.get_attribute("innerHTML")
+        dashboard_test_function(driver, kibana_url, timeout, self.dashboard_id, "Sercurity log - logon as a service - Logon type 5",".euiDataGrid",".visError")
 
-    @pytest.mark.skip(reason="Skipping this test")
     def test_security_log_logon_created_logon_type_2(self, setup_login, kibana_url, timeout):
         driver = setup_login
-        driver.get(f"{kibana_url}/app/dashboards#/view/{self.dashboard_id}")
-        expected_cond = EC.presence_of_element_located((By.CLASS_NAME, "react-grid-layout"))
-        WebDriverWait(driver, timeout).until(expected_cond)
-        panel_title = "Security log - Logon created - Logon type 2"
-        selector = f'div[data-title="{panel_title}"]'
-        expected_cond = EC.presence_of_element_located((By.CSS_SELECTOR, selector))
-        WebDriverWait(driver, timeout).until(expected_cond)
-        panel = driver.find_element(By.CSS_SELECTOR, selector)
-        assert "No results found" not in panel.get_attribute("innerHTML")
+        dashboard_test_function(driver, kibana_url, timeout, self.dashboard_id, "Security log - Logon created - Logon type 2",".tbvChart",".visError")
 
     def test_security_log_network_logon_created_type_3(self, setup_login, kibana_url, timeout):
         driver = setup_login
-        driver.get(f"{kibana_url}/app/dashboards#/view/{self.dashboard_id}")
-        expected_cond = EC.presence_of_element_located((By.CLASS_NAME, "react-grid-layout"))
-        WebDriverWait(driver, timeout).until(expected_cond)
-        panel_title = "Security log - network logon created - Logon type 3"
-        selector = f'div[data-title="{panel_title}"]'
-        expected_cond = EC.presence_of_element_located((By.CSS_SELECTOR, selector))
-        WebDriverWait(driver, timeout).until(expected_cond)
-        panel = driver.find_element(By.CSS_SELECTOR, selector)
-        assert "No results found" not in panel.get_attribute("innerHTML")
+        dashboard_test_function(driver, kibana_url, timeout, self.dashboard_id, "Security log - network logon created - Logon type 3",".tbvChart",".visError")
 
-    @pytest.mark.skip(reason="Skipping this test")
     def test_security_log_process_creation_event_id_4688(self, setup_login, kibana_url, timeout):
         driver = setup_login
-        driver.get(f"{kibana_url}/app/dashboards#/view/{self.dashboard_id}")
-        expected_cond = EC.presence_of_element_located((By.CLASS_NAME, "react-grid-layout"))
-        WebDriverWait(driver, timeout).until(expected_cond)
-        panel_title = "Security log - Process creation - event ID 4688"
-        selector = f'div[data-title="{panel_title}"]'
-        expected_cond = EC.presence_of_element_located((By.CSS_SELECTOR, selector))
-        WebDriverWait(driver, timeout).until(expected_cond)
-        panel = driver.find_element(By.CSS_SELECTOR, selector)
-        assert "No results found" not in panel.get_attribute("innerHTML")
+        dashboard_test_function(driver, kibana_url, timeout, self.dashboard_id, "Security log - Process creation - event ID 4688",".euiDataGrid",".euiDataGrid__noResults")        
 
+    def test_security_log_events(self, setup_login, kibana_url, timeout):
+        driver = setup_login
+        dashboard_test_function(driver, kibana_url, timeout, self.dashboard_id, "Security logs events",".visualization", ".dummyval")
+        # The arguement ".dummyval" is being used though it is not a valid selector. 
+        # This panel should always have a visualization so there should never be no data message displayed.
+        # If there is no visualization rendered or "No Results found" message is displayed for this panel on dashboard, this test should fail which is correct behavior
 
-
+    def test_failed_logon_type_codes(self, setup_login, kibana_url, timeout):
+        driver = setup_login
+        dashboard_test_function(driver, kibana_url, timeout, self.dashboard_id, "Failed logon type codes",".visualization", ".dummyval")
+        
+    def test_failed_logon_status_codes(self, setup_login, kibana_url, timeout):
+        driver = setup_login
+        dashboard_test_function(driver, kibana_url, timeout, self.dashboard_id, "Failed logon status codes",".visualization", ".dummyval")
