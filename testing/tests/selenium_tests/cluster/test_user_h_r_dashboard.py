c7a13fbc2ce2dc8d464f430e82bfd8341c417a50
rishagg01 <149525835+rishagg01@users.noreply.github.com>
2024-07-25 14:47:47 -0400

    Updated User HR Dashboard Selenium Test for User HR Logon Title panel (#385)
    * Updated selenium tests for USER HR dashboard panels post data insertion
    
    * Updated User HR Dashboard Selenium Test for User HR Logon Title panel

--- testing/tests/selenium_tests/cluster/test_user_h_r_dashboard.py
+++ testing/tests/selenium_tests/cluster/test_user_h_r_dashboard.py
@@ -44,11 +44,11 @@ class TestUserHRDashboard:
         # This panel should always have a visualization so there should never be no data message displayed.
         # If there is no visualization rendered or "No Results found" message is displayed for this panel on dashboard, this test should fail which is correct behavior
 
-    @pytest.mark.skip(reason="Skipping this test")
+    #@pytest.mark.skip(reason="Skipping this test")
     def test_hr_user_activity_title(self, setup_login, kibana_url, timeout):
         driver = setup_login
         dashboard_test_function(driver, kibana_url, timeout, self.dashboard_id, "HR - User activity title", ".visualization",".dummyval")
-        # This dashboard panel is not showing any data but static text. It seems this dashboard needs work.
+        
         
     def test_all_user_events_dayofweek_hourofday(self, setup_login, kibana_url, timeout):
         driver = setup_login
@@ -58,8 +58,8 @@ class TestUserHRDashboard:
         driver = setup_login
         dashboard_test_function(driver, kibana_url, timeout, self.dashboard_id, "Timestamps by Count", ".echChart",".dummyval")
         
-    @pytest.mark.skip(reason="Skipping this test")
+    #@pytest.mark.skip(reason="Skipping this test")
     def test_hr_logon_title(self, setup_login, kibana_url, timeout):
         driver = setup_login
         dashboard_test_function(driver, kibana_url, timeout, self.dashboard_id, "HR - Logon title", ".visualization",".dummyval")
-        # This dashboard panel is not showing any data but static text. It seems this dashboard needs work.
+        
