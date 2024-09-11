3c4543218574318eb88e6f6fa494885054426484
cbaxley <cbaxley@gmail.com>
2024-09-11 07:00:08 -0400

    Quote ssh commands and escape environment vars $

--- testing/v2/installers/install_v2/install.sh
+++ testing/v2/installers/install_v2/install.sh
@@ -27,7 +27,7 @@ cd "$SCRIPT_DIR/.."
 ./lib/copy_ssh_key.sh $user $hostname $password_file
 
 echo "Installing ansible"
-ssh  -o StrictHostKeyChecking=no $user@$hostname 'sudo apt-get update && sudo apt-get -y install ansible python3-pip git'
+ssh  -o StrictHostKeyChecking=no $user@$hostname 'sudo apt-get update && sudo apt-get -y install ansible python3-pip python3.10-venv git'
 
 
 # Need to set up so we can checkout a particular branch or pull down a release
