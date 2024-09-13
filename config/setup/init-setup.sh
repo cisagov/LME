8a04e33a802ec63b7a5b5509248926b784c9cf95
Michael Reeves <mreeve@sandia.gov>
2024-09-13 00:12:00 -0400

    Fix issue where permissions happen for elasticsearch data

--- config/setup/init-setup.sh
+++ config/setup/init-setup.sh
@@ -6,8 +6,10 @@ if [[ -z "${ELASTIC_PASSWORD:-}" || -z "${KIBANA_PASSWORD:-}" ]]; then
   exit 1
 fi
 
+
 CONFIG_DIR="/usr/share/elasticsearch/config"
 CERTS_DIR="${CONFIG_DIR}/certs"
+DATA_DIR="/usr/share/elasticsearch/data"
 INSTANCES_PATH="${CONFIG_DIR}/setup/instances.yml"
 
 if [ ! -f "${CERTS_DIR}/ca.zip" ]; then
@@ -23,7 +25,10 @@ if [ ! -f "${CERTS_DIR}/certs.zip" ]; then
   cat "${CERTS_DIR}/elasticsearch/elasticsearch.crt" "${CERTS_DIR}/ca/ca.crt" > "${CERTS_DIR}/elasticsearch/elasticsearch.chain.pem"
 fi
 
-echo "Setting file permissions..."
-chown -R root:root "${CERTS_DIR}"
-find "${CERTS_DIR}" -type d -exec chmod 754 {} \;
+echo "Setting file permissions... certs"
+chown -R elasticsearch:elasticsearch "${CERTS_DIR}"
+find "${CERTS_DIR}" -type d -exec chmod 755 {} \;
 find "${CERTS_DIR}" -type f -exec chmod 644 {} \;
+
+echo "Setting file permissions... data"
+chown -R elasticsearch:elasticsearch "${DATA_DIR}"
