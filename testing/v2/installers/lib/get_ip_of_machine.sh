cd3e39a7c2edf910545a7cd38b5eefe2ebd2246e
cbaxley <cbaxley@gmail.com>
2024-09-18 06:52:46 -0400

    Fail if the minimega ip isn't found

--- testing/v2/installers/lib/get_ip_of_machine.sh
+++ testing/v2/installers/lib/get_ip_of_machine.sh
@@ -8,18 +8,19 @@ get_ip() {
     /opt/minimega/bin/minimega -e .json true .filter name="$VM_NAME" vm info | jq -r '.[].Data[].Networks[].IP4'
 }
 
-echo "Waiting for IP assignment for VM: $VM_NAME"
+echo "Waiting for IP assignment for VM: $VM_NAME" >&2
 
 for ((i=1; i<=MAX_ATTEMPTS; i++)); do
     IP=$(get_ip)
 
     if [[ -n "$IP" && "$IP" != "null" ]]; then
-        echo "The IP of $VM_NAME is $IP"
+        echo $IP
         exit 0
     fi
 
-    echo "Attempt $i: No IP assigned yet. Waiting $SLEEP_INTERVAL seconds..."
+    echo "Attempt $i: No IP assigned yet. Waiting $SLEEP_INTERVAL seconds..." >&2
     sleep $SLEEP_INTERVAL
 done
 
-echo "Timeout: Failed to get IP for $VM_NAME after $MAX_ATTEMPTS attempts."
+echo "Timeout: Failed to get IP for $VM_NAME after $MAX_ATTEMPTS attempts." >&2
+exit 1
