606f957f54dad5aaf2574c2f195481dbdb913d45
Grant (SNL) <108766839+rgbrow1949@users.noreply.github.com>
2024-10-23 11:03:39 -0600

    Sysmon Install Powershell Script (#480)
    * Automatic sysmon install powershell script
    
    * Documentation on installing sysmon

---
+++ scripts/install_sysmon.ps1
@@ -0,0 +1,8 @@
+﻿# Curl and unzip sysmon off the windows sysinternals page
+curl https://download.sysinternals.com/files/Sysmon.zip -OutFile sysmon.zip
+Expand-Archive sysmon.zip
+# Curl and unzip the swift on config xml
+curl https://github.com/SwiftOnSecurity/sysmon-config/archive/refs/heads/master.zip -OutFile sysmon-config.zip
+Expand-Archive sysmon-config.zip
+# install sysmon 
+.\sysmon\sysmon -accepteula -i .\sysmon-config\sysmon-config-master\sysmonconfig-export.xml
