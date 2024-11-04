7eb98ce3beb87722d6bdafe692c33fedf6fff8ea
Rishi <rishi.aggarwal@associates.cisa.dhs.gov>
2024-11-04 15:17:30 -0500

    seltest rel2 final2

--- testing/tests/selenium_tests/cluster/test_computer_software_overview_dashboard.py
+++ testing/tests/selenium_tests/cluster/test_computer_software_overview_dashboard.py
@@ -7,36 +7,38 @@ from selenium.common.exceptions import NoSuchElementException
 from .lib import dashboard_test_function
 
 class TestComputerSoftwareOverviewDashboard:
-    dashboard_id = "33f0d3b0-8b8a-11ea-b1c6-a5bf39283f12"
-
+    #dashboard_id = "33f0d3b0-8b8a-11ea-b1c6-a5bf39283f12"
+    #dashboard_id = "new dashboard"
+    dashboard_id = "ce98c19b-587f-4d76-9c49-2e9acee257d5"
+    
     @pytest.fixture(scope="class")
     def setup_login(self, driver, login):
         login()
         yield driver
 
-    @pytest.mark.skip(reason="This test is for reference to use in 2.0")
+    #@pytest.mark.skip(reason="This test is for reference to use in 2.0")
     def test_application_crashing_and_hanging(self, setup_login, kibana_url, timeout):
         driver = setup_login
         dashboard_test_function(driver, kibana_url, timeout, self.dashboard_id, "Application Crashing and Hanging", ".echChart",".xyChart__empty")
 
-    @pytest.mark.skip(reason="This test is for reference to use in 2.0")
+    #@pytest.mark.skip(reason="This test is for reference to use in 2.0")
     def test_application_crashing_and_hanging_count(self, setup_login, kibana_url, timeout):
         driver = setup_login
         dashboard_test_function(driver, kibana_url, timeout, self.dashboard_id, "Application Crashing and Hanging Count", ".tbvChart",".visError")
 
-    @pytest.mark.skip(reason="Skipping this test")
+    #@pytest.mark.skip(reason="Skipping this test")
     def test_create_remote_threat_events(self, setup_login, kibana_url, timeout):
         driver = setup_login
-        dashboard_test_function(driver, kibana_url, timeout, self.dashboard_id, "CreateRemoteThread events", ".tbvChart",".visError")
+        dashboard_test_function(driver, kibana_url, timeout, self.dashboard_id, "CreateRemoteThread events", ".euiFlexGroup",".visError")
 
-    @pytest.mark.skip(reason="This test is for reference to use in 2.0")
+    #@pytest.mark.skip(reason="This test is for reference to use in 2.0")
     def test_filter_hosts(self, setup_login, kibana_url, timeout):
         driver = setup_login
         dashboard_test_function(driver, kibana_url, timeout, self.dashboard_id, "Filter Hosts", ".tbvChart",".visError")
 
     
-    @pytest.mark.skip(reason="This test is for reference to use in 2.0")
+    #@pytest.mark.skip(reason="This test is for reference to use in 2.0")
     def test_processes(self, setup_login, kibana_url, timeout):
         driver = setup_login
-        dashboard_test_function(driver, kibana_url, timeout, self.dashboard_id, "Processes", ".tbvChart",".visError")
-
+        dashboard_test_function(driver, kibana_url, timeout, self.dashboard_id, "Processes", ".euiDataGrid__focusWrap",".euiText")
+        
