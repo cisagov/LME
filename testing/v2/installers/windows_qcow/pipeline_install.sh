de43b5587f058872a3e104151c7aa99552abd57b
cbaxley <cbaxley@gmail.com>
2024-11-26 07:36:53 -0500

    Set up windows minimega instance in pipeline

---
+++ testing/v2/installers/windows_qcow/pipeline_install.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+#!/usr/bin/env bash
+set -e
+
+# Function to print usage
+print_usage() {
+    echo "Usage: $0 <username> <hostname> <password_file> [num_cpus] [memory_mb]"
+    echo "Required parameters:"
+    echo "  <username>: The username for the remote server"
+    echo "  <hostname>: The hostname or IP address of the remote server"
+    echo "  <password_file>: The file containing the password for the remote server"
+}
+
+# Check if all required arguments are provided
+if [ $# -lt 3 ]; then
+    print_usage
+    exit 1
+fi
+
+# Set the remote server details from the command-line arguments
+user=$1
+hostname=$2
+password_file=$3
+
+# Store the original working directory
+ORIGINAL_DIR="$(pwd)"
+
+# Get the directory of the script
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Change to the parent directory of the script
+cd "$SCRIPT_DIR/.."
+
+# Copy the SSH key to the remote machine
+./lib/copy_ssh_key.sh $user $hostname $password_file
+
+cp "./$SCRIPT_DIR/.env.example" "./$SCRIPT_DIR/.env"
+
+scp -r "$SCRIPT_DIR" ubuntu_qcow_maker $user@$hostname:/home/$user
+
+ssh $user@$hostname "cd /home/${user}/windows_qcow && sudo ./install_local.sh"
