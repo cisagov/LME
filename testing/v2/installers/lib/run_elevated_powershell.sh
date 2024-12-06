0fd1fb6e73794109f41a60160f33112b53dac93a
cbaxley <cbaxley@gmail.com>
2024-12-06 05:34:56 -0500

    Prep scripts for installing elastic agent on windows minimega

--- testing/v2/installers/lib/run_elevated_powershell.sh
+++ testing/v2/installers/lib/run_elevated_powershell.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
-run_elevated_powershell() {
-    local command="$1"
-    local host_arg="$2"
-    
-    # Use passed host, environment variable, or default value
-    HOST=${host_arg:-${WINDOWS_HOST:-"10.0.0.180"}}
-    USER="Test"
-    
-    # Add host key if not already present
-    ssh-keyscan -H $HOST >> ~/.ssh/known_hosts 2>/dev/null
-    
-    # Run the command
-    sshpass -e ssh $USER@$HOST "powershell -NoProfile -Command \"& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command \\\"${command}\\\"' -Verb RunAs -Wait}\""
-}
+#set -ex
 
-# Example usage:
-# run this first before running the below commands
-# export SSHPASS='windowspassword'
+# Check if command argument is provided
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <command> [host]"
+    echo "Example: $0 'Get-Service sshd' 10.0.0.180"
+    echo "Note: Set SSHPASS and/or WINDOWS_HOST environment variables if needed"
+    exit 1
+fi
 
-# Using default:
-# run_elevated_powershell "Get-Service sshd"
+command="$1"
+host_arg="$2"
 
-# Passing host as argument:
-# run_elevated_powershell "Get-Service sshd" "10.0.0.180"
+# Check if SSHPASS is set
+if [ -z "$SSHPASS" ]; then
+    echo "Error: SSHPASS environment variable must be set"
+    echo "Example: export SSHPASS='windowspassword'"
+    exit 1
+fi
 
-# Using environment variable:
-# export WINDOWS_HOST="10.0.0.180"
-# run_elevated_powershell "Get-Service sshd"
+# Use passed host, environment variable, or default value
+HOST=${host_arg:-${WINDOWS_HOST:-"10.0.0.180"}}
+USER="Test"
+
+# Add host key if not already present
+ssh-keyscan -H $HOST >> ~/.ssh/known_hosts 2>/dev/null
+
+# Run the command
+sshpass -e ssh $USER@$HOST "powershell -NoProfile -Command \"Start-Process PowerShell -ArgumentList '-NoProfile', '-ExecutionPolicy', 'Bypass', '-Command', '${command} *>&1 > C:\\Users\\Test\\service.txt' -Verb RunAs -Wait -PassThru; Get-Content C:\\Users\\Test\\service.txt\""
