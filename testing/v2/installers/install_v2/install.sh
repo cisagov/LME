34d965ca6e5b69fe6aafbc08866a9c45420a040e
cbaxley <cbaxley@gmail.com>
2025-02-27 10:57:37 -0500

    Checks the available versions of ansible and python

--- testing/v2/installers/install_v2/install.sh
+++ testing/v2/installers/install_v2/install.sh
@@ -26,8 +26,18 @@ cd "$SCRIPT_DIR/.."
 # Copy the SSH key to the remote machine
 ./lib/copy_ssh_key.sh $user $hostname $password_file
 
+echo "Checking ubuntu version"
+ssh -o StrictHostKeyChecking=no $user@$hostname 'cat /etc/os-release'
+
+echo "Updating apt"
+ssh -o StrictHostKeyChecking=no $user@$hostname 'sudo apt-get update'
+
+echo "Checking ansible and python version"
+ssh -o StrictHostKeyChecking=no $user@$hostname 'apt-cache policy ansible python3-pip python3-venv'
+
+
 echo "Installing ansible"
-ssh -o StrictHostKeyChecking=no $user@$hostname 'sudo apt-get update && sudo apt-get -y install ansible python3-pip python3.10-venv git && sudo locale-gen en_US.UTF-8 && sudo update-locale'
+ssh -o StrictHostKeyChecking=no $user@$hostname 'sudo apt-get -y install ansible python3-pip python3.10-venv git && sudo locale-gen en_US.UTF-8 && sudo update-locale'
 
 echo "Checking out code"
 ssh -o StrictHostKeyChecking=no $user@$hostname "cd ~ && rm -rf LME && git clone https://github.com/cisagov/LME.git"
