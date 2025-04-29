b2197ed8c409e7dc4085fdd4f875c78448065427
Clint Baxley <Clinon.Baxley@ecstech.com>
2025-04-29 09:07:55 -0400

    Adds back in the linux_only workflow and adds a script to run all the containers with act

--- scripts/wipe_lme.sh
+++ scripts/wipe_lme.sh
@@ -3,7 +3,7 @@ sudo systemctl reset-failed
 sudo systemctl disable lme.service
 sudo -i podman volume rm -a
 sudo -i podman secret rm -a
-sudo rm -rf /opt/lme /etc/lme /etc/containers/systemd
+sudo rm -rf /opt/lme /etc/containers/systemd
 sudo -i podman stop $(sudo -i podman ps -aq)
 sudo -i podman rm $(sudo -i podman ps -aq)
 rm -rf ~/.config/containers
